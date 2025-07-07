6fe30ee6407267dbf1eec39497f4369561c07242
hyoinkang <hyoin1704@gmail.com>
2025-07-07 18:16:57 +0900

    chore : swagger 설정 및 versioning
    api/v1/ prefix를 두어 versioning.
    @nestjs/swagger 설치 및 swagger.config.ts 파일로 스웨거 설정

---
+++ src/config/app.config.ts
@@ -0,0 +1,22 @@
+import { ConfigService } from "@nestjs/config"
+
+export const defaultConfig = (configService: ConfigService) => ({
+    app: {
+        version: configService.get<string>('API_VERSION'),
+        perfixes: {
+            api: 'api',
+            auth: 'auth',
+        },
+        excludeRoutes: [
+            /* TODO : auth 관련 API 버전 prefix 제외.
+            {
+                path: 'auth/v1/login',
+                method: RequestMethod.POST,
+            }
+            */
+        ],
+    },
+    swagger: {
+        path: configService.get<string>('SWAGGER_PATH') || 'docs',
+    },
+});
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 import { TransformInterceptor } from './common/interceptors/transform.interceptors';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { defaultConfig } from './config/app.config';
+import { SwaggerModule } from '@nestjs/swagger';
+import { createSwaggerConfig } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const config = defaultConfig(configService);
 
+  // versioning
+  const prefix = `${config.app.perfixes.api}/${config.app.version}`;
+  app.setGlobalPrefix(prefix, {
+    exclude: config.app.excludeRoutes,
+  });
+
+  // swagger
+  const swaggerDocConfig = createSwaggerConfig(configService);
+  const document = SwaggerModule.createDocument(app, swaggerDocConfig);
+  SwaggerModule.setup(config.swagger.path, app, document);
+
+  // 응답통일 및 예외처리
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
 
