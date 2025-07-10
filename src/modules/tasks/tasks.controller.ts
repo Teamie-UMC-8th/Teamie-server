954bbf096d1b888b074461b8ab2f4f08e9aa0564
chaechae128 <128474661+chaechae128@users.noreply.github.com>
2025-07-11 00:35:57 +0900

    [Feat]: 업무 수정 API 구현 (#25)
    * feat: 업무 수정 API 구현
    
    * fix: UpdateTaskResponseDto에 from 메서드 추가 및 변환 로직 리팩토링
    
    * fix: @Patch 경로 표현 방식 수

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Post, Req, UseGuards, Patch, Delete, Get} from '@nestjs/common';
+import { Body, Controller, Post, Param, Req, UseGuards, Patch, Delete, Get} from '@nestjs/common';
 import { Request } from 'express';
 import { TasksService } from './tasks.service';
 import { CreateTaskRequestDto } from './dtos/create-task.dto';
 import { ConfigService } from '@nestjs/config';
+import { ApiBody } from '@nestjs/swagger';
+import { ApiCommonResponse, ApiCommonErrorResponse } from '../../common/response/swagger-responce.helper';
+import { UpdateTaskRequestDto , UpdateTaskResponseDto} from './dtos/update-task.dto';
 //import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 
 @Controller('/tasks')
@@ -16,4 +19,15 @@ export class TasksController {
     const userId = this.configService.get('DEFAULT_USERID');
     return await this.tasksService.createTask(userId, dto);
   } 
+
+  @Patch('/:taskId')
+  @ApiBody({ type: UpdateTaskRequestDto })
+  @ApiCommonResponse(UpdateTaskResponseDto)
+  async updateTask(
+    @Param('taskId') taskId: number,
+    @Req() req: Request, 
+    @Body() dto: UpdateTaskRequestDto) {
+	  const userId = this.configService.get('DEFAULT_USERID'); //하드코딩
+	  return await this.tasksService.updateTask(dto, userId, taskId);
+}
 }
