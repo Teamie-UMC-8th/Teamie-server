6cd34ce84dc9edf61ef9e939d8093f019590132a
sunggyeong kang <128947069+sunggyeong@users.noreply.github.com>
2025-07-18 17:55:17 +0900

    fix: github 스크립트에 마이그레이션 런 추가, docker-compose  변경 (#53)
    workflows/deploy-dev.yml에 network_mode를 host로

--- src/modules/steps/entities/steps.entity.ts
+++ src/modules/steps/entities/steps.entity.ts
@@ -8,9 +8,6 @@ export class Step extends BaseEntity {
     @Column({ length: 20 })
     name: string;
 
-    @Column({ default: false })
-    isDeleted: boolean;
-
     @ManyToOne(() => Project, (project) => project.steps, {
         onDelete: 'CASCADE',
     })
