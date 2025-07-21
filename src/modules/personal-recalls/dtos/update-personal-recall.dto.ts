b77ffcd97a1bdbc1d97b9d3a1d979f7947bbdbb3
chowon442 <chowon442@gmail.com>
2025-07-22 01:25:12 +0900

    refactor: 개인 회고 컬럼명 수정 및 글자 수 수정
    - 글자 수 5000자에서 2000자로 수정\n- nullable true로 설정\n- 컬럼명 구체적으로 변경

--- src/modules/personal-recalls/dtos/update-personal-recall.dto.ts
+++ src/modules/personal-recalls/dtos/update-personal-recall.dto.ts
@@ -5,16 +5,16 @@ export class UpdatePersonalRecallDto {
         example: '내가 가장 잘했다고 생각하는 건 ‘전체 흐름을 잡아가는 능력’이었다...',
         description: '질문1에 대한 답변',
     })
-    Q1?: string;
+    collaborationProfile?: string;
     @ApiPropertyOptional({
         example:
             '6월 정기모임 참석률이 예상보다 낮았던 날, 준비한 조별 활동이 어색해져 현장에서 급히 프로그램을 수정해야 했다...',
         description: '질문2에 대한 답변',
     })
-    Q2?: string;
+    memorableExperience?: string;
     @ApiPropertyOptional({
         example: '강점은 ‘관계의 리듬을 설계하는 감각’과 ‘사람 중심 운영’이다...',
         description: '질문3에 대한 답변',
     })
-    Q3?: string;
+    strengthsAndGrowth?: string;
 }
