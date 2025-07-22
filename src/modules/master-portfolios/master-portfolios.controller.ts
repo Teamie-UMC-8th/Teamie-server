c72c5d35de7160ed2056ec6bde902ca60cce3d74
hyoinkang <hyoin1704@gmail.com>
2025-07-23 00:46:42 +0900

    feat: 페이징 객체 swagger 매핑을 위한 데코레이터 구현
    @ApiCommonResponseWithPagination 데코레이터를 이용하여 페이징 객체 명세 가능
    추가로, Cursor class의 위치를 컨트롤러에서 dto 내부로 이동함.

--- src/modules/master-portfolios/master-portfolios.controller.ts
+++ src/modules/master-portfolios/master-portfolios.controller.ts
@@ -2,18 +2,10 @@ import { Controller, Param, ParseIntPipe, Post, Get, Query } from '@nestjs/commo
 import { ConfigService } from '@nestjs/config';
 import { ApiBearerAuth, ApiOperation, ApiProperty, ApiTags } from '@nestjs/swagger';
 import { PaginatedResponseDto } from 'src/common/response/paginated-response.dto';
-import { UserMasterPortfoliosResponseDto } from './dtos/user-master-portfolios-response.dto';
+import { Cursor, UserMasterPortfoliosResponseDto } from './dtos/user-master-portfolios-response.dto';
 import { MasterPortfoliosService } from './master-portfolios.service';
 import { User } from 'src/common/decorators/user.decorator';
-
-class Cursor {
-    @ApiProperty({
-        required: false,
-        example: '2025-03-08',
-        description: '프로젝트 생성일자 기준(ISO 8301 형식)',
-    })
-    cursor: string;
-}
+import { ApiCommonResponseWithPagination } from 'src/common/response/swagger-response.helper';
 
 @ApiTags('MasterPortfolios')
 @ApiBearerAuth('access-token')
@@ -43,6 +35,7 @@ export class MasterPortfoliosController {
         description:
             '사용자의 마스터포트폴리오 리스트를 조회하는 API입니다. 페이징을 포함하며, 커서는 프로젝트 생성일자입니다.',
     })
+    @ApiCommonResponseWithPagination(UserMasterPortfoliosResponseDto)
     async getUsersMasterPortfolios(
         @User('id') userId: number,
         @Query() req: Cursor //NOTE: 프로젝트 생성일자 기준(ISO 8301 형식)
