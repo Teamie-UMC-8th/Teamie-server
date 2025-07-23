57d96809719f4f24efe71185d47b0434ccc8ba5a
sunggyeong kang <128947069+sunggyeong@users.noreply.github.com>
2025-07-23 15:56:56 +0900

    feat: step 수정 구현 (#86)

--- src/modules/steps/steps.controller.ts
+++ src/modules/steps/steps.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Body, ParseIntPipe, Patch, Param } from '@nestjs/common';
 import { StepsService } from './steps.service';
 import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import {
@@ -7,9 +7,28 @@ import {
 } from '../../common/response/swagger-response.helper';
 import { CreateStepDto } from './dtos/create-step.dto';
 import { User } from 'src/common/decorators/user.decorator';
+import { CommonResponse } from 'src/common/response/common-response.dto';
+import { UpdateStepDto, UpdateStepResponseDto } from './dtos/update-step.dto';
+
 @ApiTags('Steps')
 @ApiBearerAuth('access-token')
 @Controller('/steps')
 export class StepsController {
     constructor(private readonly stepsService: StepsService) {}
+    @Patch('/:stepId')
+    @ApiOperation({
+        summary: 'step 수정',
+        description: 'step의 이름을 수정합니다.',
+    })
+    @ApiBody({ type: UpdateStepDto })
+    @ApiCommonResponse(UpdateStepResponseDto)
+    @ApiCommonErrorResponse('STEP_NOT_FOUND', '해당 Step을 찾을 수 없습니다.')
+    @ApiCommonErrorResponse('UNAUTHORIZED_USER', '인증되지 않은 사용자입니다.')
+    async updateStep(
+        @Param('stepId', ParseIntPipe) stepId: number,
+        @Body() dto: UpdateStepDto,
+        @User() userId: number
+    ): Promise<CommonResponse<UpdateStepDto>> {
+        return this.stepsService.updateStep(stepId, dto, userId);
+    }
 }
