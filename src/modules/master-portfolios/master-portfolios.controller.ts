2145277ea6e083306e827b1c5821bda1457ecf3f
강효인 <103008960+hyoinkang@users.noreply.github.com>
2025-07-24 21:05:58 +0900

    [Feat] 사용자 별 마스터 포트폴리오 조회 기능 구현 (#90)
    * feat: 사용자 별 마스터포트폴리오 조회 기능 구현
    
    프로젝트 생성 일자를 기준으로 하는 커서 기반 페이징 적용
    쿼리 빌더를 사용하여 최소한으로 필요한 필드를 가져오도록 함.
    페이징에 사용될 공통 페이징 response 정의
    
    * feat: 페이징 객체 swagger 매핑을 위한 데코레이터 구현
    
    @ApiCommonResponseWithPagination 데코레이터를 이용하여 페이징 객체 명세 가능
    추가로, Cursor class의 위치를 컨트롤러에서 dto 내부로 이동함.
    
    * fix: 쿼리 오류 수정
    
    쿼리빌더의 getRawMany()를 통해 칼럼을 가져올 때 이름이 project_createdAt처럼 표시됨
    dto의 변환 과정에서 createdAt을 탐지하지 못해 undefined 에러 발생
    addSelect의 alias 설정을 통해 createdAt을 잘 탐지할 수 있도록 수정함.
    
    * chore: 포맷팅 적용
    
    * fix: nextCursor 오류 해결
    
    nextCursor 값이 현재 페이지의 마지막 createdAt이 되어야 하는데,
    hasNextPage의 판단을 위해 pageSize+1 만큼의 리스트를 가져오게 되어
    nextCursor 값이 다음 페이지의 createdAt이 되는 문제 발생,
    따라서, nextCursor를 가져오는 로직을 살짝 변경하여 오류를 해결하였다.

--- src/modules/master-portfolios/master-portfolios.controller.ts
+++ src/modules/master-portfolios/master-portfolios.controller.ts
@@ -1,11 +1,24 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ApiBearerAuth, ApiOperation, ApiProperty, ApiTags } from '@nestjs/swagger';
+import { PaginatedResponseDto } from 'src/common/response/paginated-response.dto';
+import {
+    Cursor,
+    UserMasterPortfoliosResponseDto,
+} from './dtos/user-master-portfolios-response.dto';
 import { MasterPortfoliosService } from './master-portfolios.service';
 import { User } from 'src/common/decorators/user.decorator';
 import { MasterPortfolioRequestDto } from './dtos/master-portfolio-request.dto';
+import { ApiCommonResponseWithPagination } from 'src/common/response/swagger-response.helper';
 
+@ApiTags('MasterPortfolios')
+@ApiBearerAuth('access-token')
 @Controller('master-portfolios')
 export class MasterPortfoliosController {
-    constructor(private readonly masterPortfoliosService: MasterPortfoliosService) {}
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly masterPortfoliosService: MasterPortfoliosService
+    ) {}
 
     @Post(':projectId/questions')
     async createQuestions(
@@ -46,5 +59,26 @@ export class MasterPortfoliosController {
         @Body() updateDataDto: MasterPortfolioRequestDto
     ) {
         return this.masterPortfoliosService.updateMasterPortfolio(userId, projectId, updateDataDto);
+    }
+
+    @Get('/me')
+    @ApiOperation({
+        summary: '마이페이지/사용자 별 마스터포트폴리오 조회 API',
+        description:
+            '사용자의 마스터포트폴리오 리스트를 조회하는 API입니다. 페이징을 포함하며, 커서는 프로젝트 생성일자입니다.',
+    })
+    @ApiCommonResponseWithPagination(UserMasterPortfoliosResponseDto)
+    async getUsersMasterPortfolios(
+        @User('id') userId: number,
+        @Query() req: Cursor //NOTE: 프로젝트 생성일자 기준(ISO 8301 형식)
+    ): Promise<PaginatedResponseDto<UserMasterPortfoliosResponseDto> | null> {
+        //파라미터의 기본값 처리
+        const cursorDate = req.cursor ? new Date(req.cursor) : new Date(); //NOTE: 커서의 디폴트 값은 now
+        const pageSize = Number(this.configService.get('PAGE_SIZE')) || 20;
+        return await this.masterPortfoliosService.getMasterPortfoliosByUser(
+            userId,
+            cursorDate,
+            pageSize
+        );
     }
 }
