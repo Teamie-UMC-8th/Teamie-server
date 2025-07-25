d606335b9ca0197a8d6d1f0b6009c8cfd15e1c1f
hyoinkang <hyoin1704@gmail.com>
2025-07-26 05:14:31 +0900

    feat: 일정 상세 페이지 조회 기능 구현
    sync-update 이벤트와 동일한 로직 사용
    사용자의 권한 체크는 projectsService에 있는 코드를 활용해 구현함.
    도메인 별 권한 체크는 추후 커스텀 Guard로 처리하도록 리팩토링 할 예정

--- src/modules/plans/plans.controller.ts
+++ src/modules/plans/plans.controller.ts
@@ -1,18 +1,44 @@
-import { Controller, Param, Patch } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, ParseIntPipe, Patch } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { PlansGateway } from './gateways/plans.gateway';
 import { Pulbic } from 'src/common/decorators/public.decorator';
+import { User } from 'src/common/decorators/user.decorator';
+import { ApiCommonErrorResponse, ApiCommonResponse } from 'src/common/response/swagger-response.helper';
+import { PlanDetails } from './dtos/plan-details.dto';
+import { ErrorCode } from 'src/common/exceptions/errorcode.enum';
+import { PlansService } from './plans.service';
+import { ProjectForbiddenException } from 'src/common/exceptions/custom.errors';
 
 @ApiTags('Plans')
 @ApiBearerAuth('access-token')
 @Controller('/plans')
 export class PlansController {
-    constructor(private readonly plansGateway: PlansGateway) {}
+    constructor(
+        private readonly plansGateway: PlansGateway,
+        private readonly plansService: PlansService,
+    ) {}
 
     @Pulbic()
     @Patch('/test/:planId')
     async testBroadCast(@Param('planId') planId: number) {
+        //TODO: 추후 삭제하기
         this.plansGateway.broadCastUpdate(planId, '브로드캐스트 테스트');
         return 'test';
+    }
+
+    @Get('/:planId')
+    @ApiOperation({
+        summary: '일정 상세 페이지 조회 API',
+        description: 'planId에 해당하는 일정 상세 페이지를 조회하는 API입니다.'
+    })
+    @ApiCommonResponse(PlanDetails)
+    @ApiCommonErrorResponse(ErrorCode.PLAN_NOT_FOUND, "PLAN_NOT_FOUND", 404)
+    async getDetails(
+        @Param('planId', ParseIntPipe) planId: number,
+        @User('id') userId: number
+    ): Promise<PlanDetails>{
+        const check = await this.plansService.checkPermission(userId, planId);
+        if(!check) throw new ProjectForbiddenException();
+        return this.plansService.getDetails(planId);
     }
 }
