955968584dfa9db775c39131ffc1d4a26b2955ba
강효인 <103008960+hyoinkang@users.noreply.github.com>
2025-07-27 13:31:57 +0900

    [Feat] 일정 상세 페이지 조회 기능 구현 (#115)
    * fix: 일정/시작 시간 필드 타입 변경
    
    startHour: Date에서 startHour: string {type: 'time'}으로 변경함.
    
    * feat: 일정 상세 페이지 조회 기능 구현
    
    sync-update 이벤트와 동일한 로직 사용
    사용자의 권한 체크는 projectsService에 있는 코드를 활용해 구현함.
    도메인 별 권한 체크는 추후 커스텀 Guard로 처리하도록 리팩토링 할 예정
    
    * test: 일정 상세 조회에 대한 컨트롤러/서비스 테스트 코드 추가
    
    PlansService 유닛 테스트 추가
    PlansController 유닛 테스트 추가
    
    * chore: 포맷팅 적용

--- src/modules/plans/plans.service.ts
+++ src/modules/plans/plans.service.ts
@@ -4,12 +4,14 @@ import { Plan } from './plans.entity';
 import { Repository } from 'typeorm';
 import { PlanDetails } from './dtos/plan-details.dto';
 import { PlanNotFoundException } from 'src/common/exceptions/custom.errors';
+import { ProjectsService } from '../projects/projects.service';
 
 @Injectable()
 export class PlansService {
     constructor(
         @InjectRepository(Plan)
-        private readonly plansRepository: Repository<Plan>
+        private readonly plansRepository: Repository<Plan>,
+        private readonly projectsService: ProjectsService
     ) {}
 
     async getDetails(planId: number): Promise<PlanDetails> {
@@ -24,5 +26,17 @@ export class PlansService {
             .getOne();
         if (!plan) throw new PlanNotFoundException(planId);
         return PlanDetails.from(plan);
+    }
+
+    // NOTE: 사용자의 권한 체크, Custom Guard로 추후 리팩토링 예정
+    async checkPermission(userId: number, planId: number): Promise<Boolean> {
+        const plan = await this.plansRepository.findOne({
+            where: { id: planId },
+            relations: { project: true },
+            select: { id: true },
+        });
+        if (!plan) throw new PlanNotFoundException({ planId: Number(planId) });
+        const projectId = plan?.project.id;
+        return await this.projectsService.checkProjectMembership(userId, projectId);
     }
 }
