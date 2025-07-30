fcf38c06baf33318d78e90f5358b3328ab0f6f58
chaechae128 <128474661+chaechae128@users.noreply.github.com>
2025-07-30 12:12:33 +0900

    feat 대댓글 수정 구현 (#141)

---
+++ src/modules/comments/cocomments/cocomments.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Param, Delete, Patch, Body, Req } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags, ApiOkResponse, ApiOperation, ApiBody } from '@nestjs/swagger';
+import { User } from 'src/common/decorators/user.decorator';
+import { CocommentsService } from './cocomments.service';
+import { ApiCommonResponse } from 'src/common/response/swagger-response.helper';
+import { UpdateCocommentResponseDto, UpdateCocommentRequestDto } from './dto/update-cocomment.dto';
+import { Transactional } from 'src/common/decorators/transaction.decorator';
+import { CommonResponse } from 'src/common/response/common-response.dto';
+import { TransactionalRequest } from 'src/common/decorators/transaction.decorator';
+
+@ApiTags('Cocomments')
+@ApiBearerAuth('access-token')
+@Controller('/cocomments')
+export class CocommentsController {
+    constructor(private readonly cocommentsService: CocommentsService) {}
+
+    @ApiOperation({
+        summary: '대댓글 수정',
+        description: '대댓글을 수정합니다.',
+    })
+    @ApiBody({ type: UpdateCocommentRequestDto })
+    @ApiCommonResponse(UpdateCocommentResponseDto)
+    @Transactional()
+    @Patch('/:cocommentId')
+    async updateComment(
+        @Param('cocommentId') cocommentId: number,
+        @Body() dto: UpdateCocommentRequestDto,
+        @User('id') userId: number,
+        @Req() req: TransactionalRequest
+    ) {
+        return await this.cocommentsService.updateComment(userId, cocommentId, dto, req.queryRunner);
+    }
+
+
+    
+}
