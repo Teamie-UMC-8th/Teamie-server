9af6d5d0bc7b1769dd34e988b54e6f8680f21925
chowon442 <chowon442@gmail.com>
2025-08-04 01:56:27 +0900

    feat: 선택 가능한 회의록 조회 api 구현 - api 구현 - SelectablePlanResponseDto 추가

--- src/modules/master-portfolios/master-portfolios.module.ts
+++ src/modules/master-portfolios/master-portfolios.module.ts
@@ -7,10 +7,11 @@ import { MasterPortfolio } from './entities/master-portfolios.entity';
 import { LLMModule } from 'src/infra/llm/llm.module';
 import { MasterPortfolioAI } from './entities/master-portfolio-ai.entity';
 import { Project } from '../projects/entities/projects.entity';
+import { Plan } from '../plans/entities/plan.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Questions, MasterPortfolio, MasterPortfolioAI, Project]),
+        TypeOrmModule.forFeature([Questions, MasterPortfolio, MasterPortfolioAI, Project, Plan]),
         LLMModule,
     ],
     controllers: [MasterPortfoliosController],
