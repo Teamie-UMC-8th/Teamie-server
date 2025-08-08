745907d49d329f7f8409e3eb79d29c4606befad4
chowon442 <chowon442@gmail.com>
2025-08-08 17:29:31 +0900

    feat: AI첨삭 에러 처리 수정 및 DB 실제 연결

--- src/infra/llm/llm.service.ts
+++ src/infra/llm/llm.service.ts
@@ -227,12 +227,16 @@ export class LLMService {
             name: 'searchQuery',
         });
 
+        const correctionData = await qr.manager.findOne(PortfolioCorrection, {
+            where: { id: correctionId },
+            select: ['submissionTarget', 'jobTitle', 'jd'],
+        });
+
         // TODO: 개수 제한 작동 안함. 프롬프트 개선하기
         const extractedQuery = await queryExtractionPrompt.pipe(queryExtractor).invoke({
-            companyName: '레진코믹스',
-            jobTitle: '글로벌 MD',
-            jobDescription:
-                '담당업무\n- 국내외 MD 유통 업체 관리\n- MD 수출 과정 핸들링\n-> 오더 주문, 수금, 발주, 출고 등 관리\n-> 서류 통관 인허가, 면장 등\n- 공급 매출 정산 업무 (ERP 시스템 판매 정보 및 결산)\n\n자격요건\n- 영어 비즈니스 커뮤니케이션이 가능하신 분\n- K-POP, 웹툰IP의 이해도를 가지고 계신 분\n- 문서 프로그램 엑셀 활용 능력이 높으신 분\n- 대내/외 원활한 소통 능력을 갖추신 분\n\n우대요건\n- 주도적인 목표 수립과 실행력을 보유하신 분\n- 여성향 장르 웹툰에 대한 이해도가 높으신 분\n- 대중적인 트렌드에 민감하고 시장 트렌드 분석이 가능하신 분\n- 팀워크를 중시하며 긍정적인 커뮤니케이션 마인드를 보유하신 분\n- 팬덤 비즈니스에 대한 높은 이해도를 보유하신 분\n- ERP, 상급 수준의 MS Office 활용, 도구 툴 활용이 가능하신 분 (Excel, Power Point 등)\n- 해외 출장 업무에 거부감이 없으신 분',
+            companyName: correctionData?.submissionTarget,
+            jobTitle: correctionData?.jobTitle,
+            jobDescription: correctionData?.jd,
             k: 2,
         });
 
