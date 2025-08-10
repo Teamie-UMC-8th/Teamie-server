8f794cbc3b81f834144becf7fe58e747e3a05bfb
chaechae128 <kelly021128@naver.com>
2025-08-11 00:19:13 +0900

    [docs]comment, cocomment 스웨거 명세 수정

--- src/modules/comments/cocomments/cocomments.controller.ts
+++ src/modules/comments/cocomments/cocomments.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Param, Delete, Patch, Body, Req } from '@nestjs/common';
-import { ApiTags, ApiOkResponse, ApiOperation, ApiBody } from '@nestjs/swagger';
+import { ApiTags, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { User } from 'src/common/decorators/user.decorator';
 import { CocommentsService } from './services/cocomments.service';
-import { ApiCommonResponse } from 'src/common/response/swagger-response.helper';
+import {
+    ApiCommonResponse,
+    ApiCommonErrorResponse,
+} from 'src/common/response/swagger-response.helper';
 import { UpdateCocommentResponseDto, UpdateCocommentRequestDto } from './dto/update-cocomment.dto';
 import { Transactional } from 'src/common/decorators/transaction.decorator';
 import { CommonResponse } from 'src/common/response/common-response.dto';
 import { TransactionalRequest } from 'src/common/decorators/transaction.decorator';
+import { ErrorCode } from '../../../common/exceptions/errorcode.enum';
+import { HttpStatus } from '@nestjs/common';
 
 @ApiTags('Cocomments')
 @Controller('/cocomments')
@@ -17,8 +22,17 @@ export class CocommentsController {
         summary: '대댓글 수정',
         description: '대댓글을 수정합니다.',
     })
-    @ApiBody({ type: UpdateCocommentRequestDto })
     @ApiCommonResponse(UpdateCocommentResponseDto)
+    @ApiCommonErrorResponse(
+        ErrorCode.COCOMMENT_NOT_FOUND,
+        '대댓글을 찾을 수 없습니다.',
+        HttpStatus.NOT_FOUND
+    )
+    @ApiCommonErrorResponse(
+        ErrorCode.FORBIDDEN_COCOMMENT_FOR_UPDATE,
+        '본인이 작성한 대댓글만 수정할 수 있습니다.',
+        HttpStatus.FORBIDDEN
+    )
     @Transactional()
     @Patch('/:cocommentId')
     async updateComment(
@@ -40,6 +54,16 @@ export class CocommentsController {
         description: '대댓글을 삭제합니다.',
     })
     @ApiOkResponse({ type: String, description: '대댓글 삭제 성공' })
+    @ApiCommonErrorResponse(
+        ErrorCode.FORBIDDEN_COCOMMENT_FOR_DELETE,
+        '본인이 작성한 대댓글만 삭제할 수 있습니다.',
+        HttpStatus.FORBIDDEN
+    )
+    @ApiCommonErrorResponse(
+        ErrorCode.COCOMMENT_NOT_FOUND,
+        '대댓글을 찾을 수 없습니다.',
+        HttpStatus.NOT_FOUND
+    )
     @Transactional()
     @Delete('/:cocommentId')
     async deleteCocomment(
