551f434aa3fcda486b8c31c21a45c84c449553d3
hyoinkang <hyoin1704@gmail.com>
2025-08-11 14:47:23 +0900

    feat: 구독 모델의 메인 게이트웨이 구현
    핸드쉐이크 과정에서 쿠키로 accessToken을 받아 사용자 인증/인가 처리
    subscribe: 실시간 동기화가 필요한 페이지의 유형에 따라 room에 join함
    unsubscribe: room에서 leave
    publish: room에 payload를 브로드캐스트
    이벤트리스너에서 게이트웨이를 주입받아 사용함

---
+++ src/common/enums/sub-event-type.enum.ts
@@ -0,0 +1,7 @@
+export enum SubEventType {
+    // 실시간 동기화가 필요한 페이지 별로 처리
+    PROJECT_DASHBOARD = 'project:dashboard',
+    PROJECT_CALENDER = 'project:calender',
+    PLAN_DETAIL = 'plan:notification',
+    TASK_DETAIL = 'task:notification',
+}
