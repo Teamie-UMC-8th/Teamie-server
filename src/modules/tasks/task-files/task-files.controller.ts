80a3423813c494a625cb05b84f1730b70f3bac3a
chaechae128 <128474661+chaechae128@users.noreply.github.com>
2025-08-13 12:39:19 +0900

    [Docs] task, comment, cocomment, taskFile 스웨거 명세 추가 (#271)
    * [docs]comment, cocomment 스웨거 명세 수정
    
    * [Docs] comment, cocomment, taskFile 스웨거 명세 추가
    
    * [Docs] comment, cocomment, taskFile 스웨거 명세 추가
    
    * [Docs] 업무 수정 스웨거 명세 수정

--- src/modules/tasks/task-files/task-files.controller.ts
+++ src/modules/tasks/task-files/task-files.controller.ts
@@ -2,6 +2,12 @@ import { Controller, Param, Delete, Req } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { User } from 'src/common/decorators/user.decorator';
 import { TaskFilesService } from './task-files.service';
+import { ErrorCode } from '../../../common/exceptions/errorcode.enum';
+import { HttpStatus } from '@nestjs/common';
+import {
+    ApiCommonErrorResponse,
+} from 'src/common/response/swagger-response.helper';
+
 import { Transactional } from 'src/common/decorators/transaction.decorator';
 import { TransactionalRequest } from 'src/common/decorators/transaction.decorator';
 import { CommonResponse } from 'src/common/response/common-response.dto';
@@ -17,6 +23,11 @@ export class TaskFilesController {
         description: '업무의 파일을 삭제합니다.',
     })
     @ApiOkResponse({ type: String, description: '업무 파일 삭제 성공' })
+    @ApiCommonErrorResponse(
+        ErrorCode.TASK_FILE_NOT_FOUND,
+        '업무파일을 찾을 수 없습니다.',
+        HttpStatus.NOT_FOUND
+    )
     @Transactional()
     @Delete('/:taskFileId')
     async deleteTaskFile(
