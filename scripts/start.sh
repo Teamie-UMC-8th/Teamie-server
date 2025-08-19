031b2f635df884633f338b832b1be4aaa973056f
sunggyeong kang <128947069+sunggyeong@users.noreply.github.com>
2025-08-19 17:49:17 +0900

    [Chore] 무중단 배포 워크플로 작성 (#358)
    * chore: 무중단 배포 워크플로 작성
    
    * fix: teamie 오타 수정
    
    * fix: cd 이동 명령어 삭제
    
    * fix: app pull 수정
    
    * fix: migrate:run 부분 삭제

--- scripts/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/teamie
-
-# Docker 빌드 및 재시작
-docker-compose -f docker-compose.prod.yml down
-docker-compose -f docker-compose.prod.yml build
-docker-compose -f docker-compose.prod.yml up -d
